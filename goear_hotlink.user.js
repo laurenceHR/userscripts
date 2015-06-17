a63e6bcfc9b5d7f5252b45f409b6ededc38c4384
Laurence HR <laurencehr@gmail.com>
2015-06-16 23:29:25 -0500

    Nueva Version GoEar

--- goear_hotlink.user.js
+++ goear_hotlink.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name GoEar HotLink 2014 
+// @name GoEar HotLink 2015 
 // @author laurenceHR
+// @include *goear.com/listen/*
 // @include *goear.com/listen/*/*
-// @version 1.3
+// @version 1.5
 // @description Goear HotLink for download
 // ==/UserScript==
 
-// Script by Laurence HR - www.daxes.net
+// Script by laurenceHR - www.daxes.net
 
 ////// Get Song ID
 var url = document.URL;
@@ -20,19 +21,21 @@ addHotLink(hotlink);
 
 /******  Function For Add Link *****/
 function addHotLink(link){
-    var style = document.createElement('style');
-    var css = "#main.listen .actions .down {background-position-y: 8px;}" + "\n";
-        style.textContent= css;
-    var head = document.getElementsByTagName('head')[0];
-        head.appendChild(style);
+    //var style = document.createElement('style');
+    //var css = "#main.listen .actions .down {background-position-y: 8px;}" + "\n";
+    //    style.textContent= css;
+    //var head = document.getElementsByTagName('head')[0];
+    //    head.appendChild(style);
     var a = document.createElement('a');
-    	a.href = link;
-    	a.target = "_blank";
-    	a.innerHTML = 'HotLink'
-    	a.className = 'btn clear pict down';
+        a.href = link;
+        a.target = "_blank";
+        a.innerHTML = '<span class="glyphicons download"></span>';
+        a.title = 'Get HotLink';
+        a.className = '';
         a.id = 'hotlink';
     var li = document.createElement('li');
-    	li.appendChild(a);
+        li.className = 'action_item';
+        li.appendChild(a);
     var ul_actions = document.getElementsByClassName('actions')[0];
-    	ul_actions.appendChild(li);  
+        ul_actions.appendChild(li);  
 }
